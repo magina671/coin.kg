2e4850456b1718ba4ae1f177a4b6625bde7fb19a
Sula <magina70@gmail.com>
2020-04-24 13:07:28 +0600

    remade about us page

--- src/components/pages/about-us/about-us.js
+++ src/components/pages/about-us/about-us.js
@@ -1,8 +1,9 @@
 import React from "react";
 import style from "./about-us.module.css";
 import Header from "../../header";
-import Picture1 from "../../../images/about_us_2.png";
-import Picture2 from "../../../images/about_us_1.jpeg";
+import picture1 from "../../../images/about_us_1.png";
+import picture2 from "../../../images/about_us_2.png";
+import picture3 from "../../../images/about_us_3.png";
 
 export const Title = (text) => {
   return (
@@ -19,34 +20,69 @@ const AboutUs = () => {
     <div className={style.wrapper}>
       <Header />
       <Title text={"О нас"} />
-      <div className={style.content_wrapper}>
-        <div className={style.content}>
+      <div className={style.slogan}>
+        <p>Кто мы</p>
+        <span>
           Первая МикроФинансовая Компания основана Агентством Ага Хана по
           Микрофинансированию, штаб-квартира которой находится в Женеве
-          (Швейцарии). Агентство Ага Хана по Микрофинансированию является одним
-          из подразделений Организации Ага Хана по Развитию. Первая
-          МикроФинансовая Компания начала свою работу в 2003 году в рамках
-          проекта в отдаленных регионах Алай и Чон-Алай. В октябре 2006 года
-          была официально зарегистрирована как микрокредитная компания. В
-          сентябре 2017 года компания преобразовалась в микрофинансовую компанию
-          и получила лицензию Национального Банка Кыргызской Республики.
-          <br />
-          <br />
-          <span>
-            За 15 лет работы компания уверенно прошла стабильный путь развития
-            от Программы МикроФинансирования Ага Хана до успешной трансформации
-            в одну из крупнейших микрофинансовых организаций в стране с общими
-            активами более 1 млрд. сомов и клиентской базой более 22 тысяч
-            человек. В настоящее время Биринчи МФК имеет уже 15 филиалов, 10 из
-            которых находятся в Ошской области и 3 - в Нарынской, 1 - в Бишкеке
-            и 1 в Джалал-Абаде. Микрокредиты выдаются для развития таких
-            стратегических для республики областей, как животноводство и
-            сельское хозяйство, торговля, малый и средний бизнес, улучшение
-            жилищных условий и образование.
-          </span>
+        </span>
+      </div>
+
+      <div className={style.info_wrapper}>
+        <div className={style.info}>
+          <p className={style.info_number}>01</p>
+          <p className={style.info_title}>
+            Подразделение Организации Ага Хана по развитию.
+          </p>
+          <p className={style.info_description}>
+            Первая МикроФинансовая Компания начала свою работу в 2003 году в
+            рамках проекта в отдаленных регионах Алай и Чон-Алай. В октябре 2006
+            года была официально зарегистрирована как микрокредитная компания. В
+            сентябре 2017 года компания преобразовалась в микрофинансовую
+            компанию и получила лицензию Национального Банка Кыргызской
+            Республики.
+          </p>
         </div>
-        <img src={Picture1} className={style.picture1} alt="content" />
-        <img src={Picture2} className={style.picture2} alt="content" />
+        <div className={style.picture_wrapper}>
+          <img src={picture1} className={style.image} alt="first_about_us" />
+          <div className={style.substrate}></div>
+        </div>
+      </div>
+
+      <div className={style.info_wrapper}>
+        <div className={style.picture_wrapper_2}>
+          <img src={picture2} className={style.image} alt="first_about_us" />
+          <div className={style.substrate_2}></div>
+        </div>
+        <div className={style.info}>
+          <p className={style.info_number_2}>02</p>
+          <p className={style.info_title}>
+            Крупнейшая микрофинансовая организация в стране
+          </p>
+          <p className={style.info_description}>
+            Стабильный путь за 15 лет работы развития от Программы
+            МикроФинансирования Ага Хана до успешной трансформации в одну из
+            крупнейших микрофинансовых организаций в стране с общими активами
+            более 1 млрд. сомов и клиентской базой более 22 тысяч человек.
+          </p>
+        </div>
+      </div>
+
+      <div className={style.info_wrapper}>
+        <div className={style.info}>
+          <p className={style.info_number_3}>03</p>
+          <p className={style.info_title}>Филиалы и стратегические области</p>
+          <p className={style.info_description}>
+            15 филиалов, 10 из которых находятся в Ошской области и 3 - в
+            Нарынской, 1 - в Бишкеке и 1 в Джалал-Абаде. Микрокредиты выдаются
+            для развития т животноводство и сельское хозяйство, торговля, малый
+            и средний бизнес, улучшение жилищных условий и образование.
+          </p>
+        </div>
+        <div className={style.picture_wrapper}>
+          <img src={picture3} className={style.image} alt="first_about_us" />
+          <div className={style.substrate_3}></div>
+        </div>
       </div>
     </div>
   );
